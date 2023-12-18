ec02d474e5557d14658b927592499eca64713fd0
Gatera-Bosco <gbosky82@gmail.com>
2023-12-18 16:09:34 +0200

    init commit

---
+++ src/main/java/rw/mis/employee/employeemanagamentsystem/Extension/CustomException.java
@@ -0,0 +1,8 @@
+package rw.mis.employee.employeemanagamentsystem.Extension;
+
+
+public class CustomException extends RuntimeException{
+    public CustomException(String message) {
+        super(message);
+    }
+}
